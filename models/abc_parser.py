1d2fc06c823c727f56e617fb10f6a9fb53837790
Kaid <kaid@kaid.me>
2023-08-02 23:06:37 +0800

    Fix back propagation.

--- models/abc_parser.py
+++ models/abc_parser.py
@@ -1,6 +1,6 @@
 import numpy as np
 from enum import Enum
-from typing import cast, Callable
+from typing import Callable
 
 
 def generate_weight(x: int, y: int) -> np.ndarray:
@@ -20,7 +20,7 @@ def sigmoid(x: np.ndarray) -> np.ndarray:
 
 
 def mse(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-    return cast(np.mean((y_true - y_pred) ** 2), float)
+    return np.mean((y_true - y_pred) ** 2)
 
 
 class Values(Enum):
@@ -33,9 +33,9 @@ class Values(Enum):
 
 
 class AbcModel:
-    weight1: np.ndarray
-    weight2: np.ndarray
-    activation: Callable[[np.ndarray], np.ndarray]
+    weight1: np.ndarray  # 输入到隐藏层的权重矩阵
+    weight2: np.ndarray  # 隐藏层到输出层的权重矩阵
+    activation: Callable[[np.ndarray], np.ndarray]  # 激活函数
 
     def __init__(
             self,
@@ -70,18 +70,20 @@ class AbcModel:
         # 输出层误差
         output_error = output - y_true
 
-        # 输出层误差梯度
-        output_error_gradient = output_error * (output * (1 - output))
-
         # 隐藏层误差
-        hidden_error = np.dot(output_error_gradient, self.weight2.T)
+        hidden_error = np.multiply(
+            np.transpose(np.dot(self.weight2, np.transpose(output_error))),
+            np.multiply(hidden, 1 - hidden))
 
         # 隐藏层误差梯度
-        hidden_error_gradient = hidden_error * (hidden * (1 - hidden))
+        hidden_error_gradient = np.dot(np.transpose(input_data), hidden_error)
+
+        # 输出层误差梯度
+        output_error_gradient = np.dot(np.transpose(hidden), output_error)
 
         # 更新权重
-        self.weight2 += learning_rate * np.dot(hidden.T, output_error_gradient)
-        self.weight1 += learning_rate * np.dot(input_data.T, hidden_error_gradient)
+        self.weight1 -= learning_rate * hidden_error_gradient
+        self.weight2 -= learning_rate * output_error_gradient
 
         return mse(y_true, output)
 
@@ -97,7 +99,7 @@ class AbcModel:
 
         for _ in range(epoch):
             epoch_loss_list = []
-            for input_data, i in enumerate(input_dataset):
+            for i, input_data in enumerate(input_dataset):
                 label = y_true[i]
                 output_data = self.forward(input_data)
                 epoch_loss_list.append(mse(label, output_data))
